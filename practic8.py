1c7f1ac5630c4a89f419accbf72232c6b21cc121
AndreyKraynov <91223935+AndreyKraynov@users.noreply.github.com>
2024-02-02 23:34:29 +0300

    Add files via upload

---
+++ practic8.py
@@ -0,0 +1,28 @@
+
+x = 6
+print('дратути')
+
+if x < 0:
+     print('меньше нуля')
+print('дотвидания')
+a, b = 10, 16
+if a > b:
+     print('a>b')
+
+if a > b and a > 0:
+     print('успех1')
+
+if (a > b) and (a > 0 or b < 1000):
+     print('успех2')
+
+if 5 < b and b < 10:
+     print('успех3')
+
+#if '6' < 7:
+ #    print('успех')
+
+#if [5, 6] > 5:
+  #   print('успех')
+
+if '6' != 7:
+     print('успех5')
